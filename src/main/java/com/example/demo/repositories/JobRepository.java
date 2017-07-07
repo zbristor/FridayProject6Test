fa31ed25136ecf789e368eb8dc963bdee1f01b6c
zbristor <zbristor@gmail.com>
2017-07-07 16:11:39 -0400

    job search works

--- src/main/java/com/example/demo/repositories/JobRepository.java
+++ src/main/java/com/example/demo/repositories/JobRepository.java
@@ -13,4 +13,6 @@ public interface JobRepository extends CrudRepository<Job, Long> {
     @Query(value = "select distinct job.id, job.description, job.recruiter, job.skills, job.salary, job.title from job,user,skills where skills.email=? and job.skills=skills.skill;",nativeQuery = true)
     public List<Job> findAllBySkills(String username);
 
+    List<Job> findByTitle(String title);
+
 }
